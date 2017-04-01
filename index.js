888828386eea77a01bfb19dd4eaa3ea70b5a4fd0
Harrison Harnisch <hharnisc@gmail.com>
2017-04-01 16:07:46 -0500

    stub in create endpoint

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ const grpc = require('grpc');
 
 const proto = grpc.load('./user.proto');
 
-console.log('proto', proto);
+const create = (call, cb) => {
+  console.log('call', call);
+  cb(null, {
+    id: 'id',
+    email: 'test@test.com',
+    emails: ['test@test.com'],
+    providers: {
+      google: {
+        accessToken: '123',
+        refressToken: '456'
+      }
+    },
+    roles: ['read', 'write']
+  })
+};
+
+const server = new grpc.Server();
+server.addProtoService(proto.user.User.service, {create});
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+server.start();
