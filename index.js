3c76075fa24979a3267a21cb69e988a20db52bda
Harrison Harnisch <hharnisc@gmail.com>
2017-04-02 09:10:07 -0500

    add babel and eslint

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-const grpc = require('grpc');
-
-const proto = grpc.load('./user.proto');
-
-const create = (call, cb) => {
-  console.log('call', call);
-  cb(null, {
-    id: 'id',
-    email: 'test@test.com',
-    emails: ['test@test.com'],
-    providers: {
-      google: {
-        accessToken: '123',
-        refressToken: '456'
-      }
-    },
-    roles: ['read', 'write']
-  })
-};
-
-const server = new grpc.Server();
-server.addProtoService(proto.user.User.service, {create});
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-server.start();
+require('babel-register');
+require('./app');
