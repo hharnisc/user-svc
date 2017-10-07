1e57b3db74ff563f238fde8ccce81c0f4a7411f7
Harrison Harnisch <hharnisc@gmail.com>
2017-10-07 14:14:28 -0500

    better handling of mongodb connection

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ const {
  method
 } = require('@hharnisc/micro-rpc')
 
+const connectDBMiddleware = async (uri, next) => {
+  const db = await MongoClient.connect(uri)
+  return (req, res) => {
+    req.db = db.collection('users')
+    return next(req, res)
+  }
+}
+
 const main = async () => {
-  const db = await MongoClient.connect(process.env.MONGO_DB)
-  const collection = db.collection('user')
-  const server = micro(
+  const handler = await connectDBMiddleware(
+    process.env.MONGO_DB,
     rpc(
       method('create', () => 'create'),
-      method('get', () => collection.find().toArray()),
+      method('get', ({db}) => db.find().toArray()),
       method('update', () => 'update'),
       method('delete', () => 'delete'),
       method('verify', () => 'verify'),
     )
   )
-
-  server.listen(process.env.PORT)
+  const server = micro(handler)
+  server.listen(process.env.PORT, () => {
+    console.log(`Listening On Port ${process.env.PORT}`)
+  })
 }
 
 main()
