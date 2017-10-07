5e0fe4acfddadac4539a4ccbe4af833db3cc9457
Harrison Harnisch <hharnisc@gmail.com>
2017-10-07 13:55:01 -0500

    connect to mongo and run a basic query

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 ENV NODE_ENV production
+ENV PORT 80
 
 COPY packages/server/package.json /usr/src/app
 RUN npm i
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+const micro = require('micro')
+const {MongoClient} = require('mongodb')
 const {
  rpc,
  method
 } = require('@hharnisc/micro-rpc')
 
-module.exports = rpc(
-  method('create', () => 'create'),
-  method('get', () => 'get'),
-  method('update', () => 'update'),
-  method('delete', () => 'delete'),
-  method('verify', () => 'verify'),
-)
+const main = async () => {
+  const db = await MongoClient.connect(process.env.MONGO_DB)
+  const collection = db.collection('user')
+  const server = micro(
+    rpc(
+      method('create', () => 'create'),
+      method('get', () => collection.find().toArray()),
+      method('update', () => 'update'),
+      method('delete', () => 'delete'),
+      method('verify', () => 'verify'),
+    )
+  )
+
+  server.listen(process.env.PORT)
+}
+
+main()
