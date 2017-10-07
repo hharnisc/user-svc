6981cb965c708d09f37f91da7036d50e4db9b708
Harrison Harnisch <hharnisc@gmail.com>
2017-10-07 11:23:30 -0500

    stub in methods

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const {
+ rpc,
+ method
+} = require('@hharnisc/micro-rpc')
+
+module.exports = rpc(
+  method('create', () => 'create'),
+  method('get', () => 'get'),
+  method('update', () => 'update'),
+  method('delete', () => 'delete'),
+  method('verify', () => 'verify'),
+)
