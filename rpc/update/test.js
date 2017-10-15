d25c5dea1002f59140420d55af1c8a038a05812f
Harrison Harnisch <hharnisc@gmail.com>
2017-10-15 11:18:54 -0500

    should not update a user who does not exist

--- rpc/update/test.js
+++ rpc/update/test.js
@@ -100,4 +100,29 @@ describe('rpc/update', () => {
         updated: true
       })
   })
+
+  it('should not update a user who does not exist', async () => {
+    const email = 'test@test.com'
+    const dataId = '1234'
+    const data = {test: true}
+    const expectedResult = {
+      matchedCount: 0
+    }
+    const db = {
+      updateOne: jest.fn(() => Promise.resolve(expectedResult))
+    }
+    const result = await update({email, dataId, data}, {db})
+    expect(db.updateOne)
+      .toBeCalledWith({
+        email
+      }, {
+        $set: {
+          [`data.${dataId}`]: data
+        }
+      })
+    expect(result)
+      .toEqual({
+        updated: false
+      })
+  })
 })
