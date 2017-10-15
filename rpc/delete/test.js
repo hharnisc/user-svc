b2fe8373a91270c10f80605c45bc28b471b8e86a
Harrison Harnisch <hharnisc@gmail.com>
2017-10-14 20:15:40 -0500

    should not delete a user who does not exist

--- rpc/delete/test.js
+++ rpc/delete/test.js
@@ -41,4 +41,21 @@ describe('rpc/delete', () => {
         .toBe(true)
     }
   })
+
+  it('should not delete a user who does not exist', async () => {
+    const email = 'test@test.com'
+    const expectedResult = {
+      deletedCount: 0
+    }
+    const db = {
+      deleteOne: jest.fn(() => Promise.resolve(expectedResult))
+    }
+    const result = await deleteUser({email}, {db})
+    expect(db.deleteOne)
+      .toBeCalledWith({
+        email
+      })
+    expect(result)
+      .toEqual(false)
+  })
 })
